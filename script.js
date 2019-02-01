cb391de0cae3c4d4a2518acdd593ef3fa8681b16
jacheg <jacchegia@gmail.com>
2019-01-31 16:31:09 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+//spell #1
+
+$(".fade").click(function(){
+    $("#broom").fadeToggle();
+});
+
+//spell #2
+
+$(".slide").click(function(){
+    $("#aligator").slideToggle();
+});
+
+//spell #3
+
+$(".toggle").click(function(){
+    $("#goofy").toggle();
+});
